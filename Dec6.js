7bdd1023e5d64de88d22f6b10e12b89d0432351c
A. Scott Tribble <a.scott.tribble@gmail.com>
2017-12-06 14:30:15 -0800

    LOTR beginning

--- Dec6.js
+++ Dec6.js
@@ -223,26 +223,96 @@
 // Redo your Cracking the Code problem from String Drills but this time use an object as your cipher. Additionally, instead of having the function accept a single word, have the function accept a single string of words, and then return the fully decoded message.
 
 
-let decode = {
-    a: 2,
-    b: 3,
-    c: 4,
-    d: 5
-}
-// console.log(decode.a);
+// let decode = {
+//     a: 2,
+//     b: 3,
+//     c: 4,
+//     d: 5
+// }
+// // console.log(decode.a);
 
-function decodedString(str) {
-    let decodedAnswer = '';
-    let wordArray = str.split(" ");
-    wordArray.forEach(function (word) {
-        let charNumber = decode[word.substring(0, 1)];
-        if (charNumber === undefined) {
-            decodedAnswer += ' ';
-        } else {
-            decodedAnswer += word.charAt(charNumber - 1);
+// function decodedString(str) {
+//     let decodedAnswer = '';
+//     let wordArray = str.split(" ");
+//     wordArray.forEach(function (word) {
+//         let charNumber = decode[word.substring(0, 1)];
+//         if (charNumber === undefined) {
+//             decodedAnswer += ' ';
+//         } else {
+//             decodedAnswer += word.charAt(charNumber - 1);
+//         }
+//     })
+//     return decodedAnswer;
+// }
+
+// console.log(decodedString("craft block argon meter bells brown croon droop"));
+
+// 7. Factory Functions with LOTR
+
+// Write a factory function called createCharacter (review in this assignment) that could appropriately build characters from LOTR that have the following attributes:
+// ===============================================================================================
+// | Name                      | Nickname    | Race       | Origin         | Attack   | Defense  |
+// -----------------------------------------------------------------------------------------------
+// | Gandalf the White         | gandalf     | Wizard     | Middle Earth   | 10       | 6        |
+// -----------------------------------------------------------------------------------------------
+// | Bilbo Baggins             | bilbo       | Hobbit     | The Shire      | 2        | 1        |
+// -----------------------------------------------------------------------------------------------
+// | Frodo Baggins             | frodo       | Hobbit     | The Shire      | 3        | 2        |
+// -----------------------------------------------------------------------------------------------
+// | Aragorn son of Arathorn   | aragorn     | Man        | Dunnedain      | 6        | 8        |
+// -----------------------------------------------------------------------------------------------
+// | Legolas                   | legolas     | Elf        | Woodland Realm | 8        | 5        |
+// -----------------------------------------------------------------------------------------------
+// Each character should have the method describe which takes no parameters and prints out the string: "{name} is a {race} from {origin}."
+
+// Each character should also have a method called evaluateFight that takes in a character object and returns the following string: "Your opponent takes {x} damage and you receive {y} damage" where x and y are the differences between each characters attack and defense values. If defense exceeds attack, then take zero damage.
+
+// Using array literal syntax, create an array characters that calls your factory function for each character in the table above with the relevant parameters. Your characters array should now have 5 objects in it.
+
+// Add a new character to characters (make up any attributes not provided):
+
+// Arwen Undomiel is a Half-Elf of Rivendell
+// Using the .find() function, retrieve your character nicknamed aragorn from characters and then call his describe method.
+
+// Using the .filter() function, create a new array from characters that ONLY contains characters of the race Hobbit.
+
+// Using the .filter() function, create a new array from characters that ONLY contains characters with attack value above 5.
+
+// What if you wanted to equip a weapon for each character and change how they are described? For example:
+
+// Gandolf the White is a Wizard of the Middle Earth who uses a wizard staff
+// Bilbo Baggings is a Hobbit of the Shire who uses the Ring
+// Frodo ... String and Barrow Blade
+// Aragon .... Anduril
+// Legolas ... Bow and Arrow
+// Arwen .... Hadhafang
+// How would you change the factory function and other methods?
+
+function createCharacter(name, nickName, race, origin, attack, defense){
+    return {
+        name: 'string',
+        nickName: 'string',
+        race: 'string',
+        origin:'string',
+        attack: 10,
+        defense: 10,
+        describe: function(){
+            console.log(`${this.name} is a ${this.race} from ${this.origin}.`),
+        evaluateFight: function(opponent){
+            let opponentDamage = opponent.defense - this.attack;
+            let ownDamage = this.defense - opponent.attack;
+            if(opponentDamage < 0){
+                opponentDamage = 0
+                };
+            if(ownDamage < 0) {
+                ownDamage = 0
+            };   
+           console.log(`Your opponent takes ${opponentDamage} damage and you receive ${ownDamage} damage`);
+            };
+        }    
+
         }
-    })
-    return decodedAnswer;
+    }
 }
 
-console.log(decodedString("craft block argon meter bells brown croon droop"));
+let characters = []
