659ad306b4f3fb500eae8b8badfe52fa03cfaa64
John Castillo <JohnluisCastillo@gmail.com>
2017-12-06 12:41:06 -0500

    Finished Deleting keys

--- Dec6.js
+++ Dec6.js
@@ -40,17 +40,34 @@
 
 // So, for instance, if firstName was 'Jane' and lastName was 'Doe', fullName() should return 'Jane Doe'.
 
-function personMaker() {
-    var person = {
-      firstName: 'Paul',
-      lastName: 'Jones',
-      // replace `null` with a function that uses self reference to return
-      // full name
-      fullName: function(){
-          return `${this.firstName} ${this.lastName}`;
-        }   
-    }
-    return person;
-  }
+// function personMaker() {
+//     var person = {
+//       firstName: 'Paul',
+//       lastName: 'Jones',
+//       // replace `null` with a function that uses self reference to return
+//       // full name
+//       fullName: function(){
+//           return `${this.firstName} ${this.lastName}`;
+//         }   
+//     }
+//     return person;
+//   }
 
-  personMaker();
+//   personMaker();
+
+// Modify the keyDeleter function so that it deletes keys for foo and bar for any object passed in and then returns the modified object.
+
+// function keyDeleter(obj) {
+//     delete obj.foo;
+//     delete obj.bar;
+//     return obj;
+//   }
+  
+//   var sampleObj = {
+//     foo: 'foo',
+//     bar: 'bar',
+//     bizz: 'bizz',
+//     bang: 'bang',
+//   };
+
+//   console.log(keyDeleter(sampleObj));
