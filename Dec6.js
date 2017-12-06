5fbd324cd5c79ade992f08ebce32522cc4fcb909
A. Scott Tribble <a.scott.tribble@gmail.com>
2017-12-06 09:35:04 -0800

    personMaker drill

--- Dec6.js
+++ Dec6.js
@@ -34,3 +34,23 @@
 //     shade: 'blue'
 // }
 // console.log(updateObject(hi));
+
+// Self-reference
+// Modify personMaker.fullName to be a function that uses self-reference (via this) in order to return the firstName and lastName properties separated by a space.
+
+// So, for instance, if firstName was 'Jane' and lastName was 'Doe', fullName() should return 'Jane Doe'.
+
+function personMaker() {
+    var person = {
+      firstName: 'Paul',
+      lastName: 'Jones',
+      // replace `null` with a function that uses self reference to return
+      // full name
+      fullName: function(){
+          return `${this.firstName} ${this.lastName}`;
+        }   
+    }
+    return person;
+  }
+
+  personMaker();
