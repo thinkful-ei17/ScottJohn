c78cc60f5fc3135f3d1110bbc997f90d57a96770
A. Scott Tribble <a.scott.tribble@gmail.com>
2017-12-06 11:26:33 -0800

    object drills 3

--- Dec6.js
+++ Dec6.js
@@ -106,3 +106,15 @@
 // for(let key in hi){
 //     console.log(key, hi[key]);
 // }
+
+// Arrays in objects
+
+// Create an object with a property called meals which is an array of strings: 'breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'.
+// Use console.log to show the name of a hobbit's fourth meal of the day.
+// Don't forget that humans and hobbits count from 1, but computers count from 0.
+
+const hobbitMeals = {
+    meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+
+console.log(hobbitMeals.meals[3]);
